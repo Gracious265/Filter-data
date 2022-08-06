777d80755daec2d7a866773f52c04ebf00385b5f
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-08-06 13:16:01 +0200

    search function added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState} from 'react';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -8,14 +9,16 @@ import {data} from './data.js';
 
 
 function App() {
-  console.log(data);
+  const [search, setSearch] = useState('');
+  console.log(search);
+
   return (
     <div className="App">
       <Container>
         <h1 className='text-center mt-4'>Contact Keeper</h1>
         <Form>
           <InputGroup className='my-3'>
-            <Form.Control placeholder='Search Cntacts' />
+            <Form.Control onChange={(e) => setSearch(e.target.value)} placeholder='Search Cntacts' />
           </InputGroup>
         </Form>
         <Table striped bordered hover>
@@ -28,12 +31,18 @@ function App() {
           </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Jim</td>
-              <td>Nguwo</td>
-              <td>graciouscodes@gmail.com</td>
-              <td>+26590808989</td>
+
+          {data.filter((item) => {
+            return search.toLowerCase() === "" ? item : item.first_name.toLowerCase().includes(search);
+          }).map((item) => (
+            <tr key={item.id}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
             </tr>
+          ))}
+            
           </tbody>
         </Table>
       </Container>
